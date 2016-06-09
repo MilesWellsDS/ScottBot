422a6613d15a7fa5038bf42005b6336f6b405dc0
unknown <miles.wells@MWELLS-M81.corp.sd.local>
2016-06-09 11:28:54 -0400

    added some comments. altered business string to allow first letter to be capitalized.

--- index.js
+++ index.js
@@ -48,27 +48,40 @@ server.start((err) => {
     console.log('Server running at:', server.info.uri);
 });
 
+//get a business language string
 function getBusinessString() {
     var Scott = 'Scott says to '; //instantiate phrase
-    Scott += buildBusinessString() + '.';
+    Scott += buildBusinessString(false) + '.';
     return Scott;
 }
 
+//get a list of 3-5 business language strings.
 function getTodoList() {
     var numitems = Math.floor(Math.random() * 3) + 3; //Random number of items between 3-5
     var Scott = 'Scott would like you to integrate these dynamic opportunities:\n';
     for(var i = 0; i < numitems; i++) {
-        Scott += (i + 1) + ') ' + buildBusinessString() + '.\n';
+        Scott += (i + 1) + ') ' + buildBusinessString(true) + '.\n';
     }
 
     return Scott;
 }
 
-function buildBusinessString() {
+//builds the business language string.
+//if caps is true, then the first letter of the string will be capitalized.
+function buildBusinessString(caps) {
     var str = '';
-    str += adverbs[Math.floor(Math.random() * adverbs.length)] + ' '; //add adverb and space to phrase
+    if(caps)
+        str += adverbs[Math.floor(Math.random() * adverbs.length)].capitalizeFirstLetter() + ' '; //add adverb and space to phrase
+    else
+        str += adverbs[Math.floor(Math.random() * adverbs.length)] + ' '; //add adverb and space to phrase
     str += verbs[Math.floor(Math.random() * verbs.length)] + ' '; //add verb and space to phrase
     str += adjectives[Math.floor(Math.random() * adjectives.length)] + ' '; //add adjective and space to phrase
     str += nouns[Math.floor(Math.random() * nouns.length)]; //add noun to phrase
     return str;
+}
+
+//capitalizes the first character in a string.
+//taken from Steve Harrison http://stackoverflow.com/questions/1026069/capitalize-the-first-letter-of-string-in-javascript
+String.prototype.capitalizeFirstLetter = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
 }
