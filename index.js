75580133601119c20f88f85160e0912c5c76541d
unknown <miles.wells@MWELLS-M81.corp.sd.local>
2016-06-09 13:35:36 -0400

    Added scottbotspeech route.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const adjectives = require('./words/adjectives');
 const adverbs = require('./words/adverbs');
 const nouns = require('./words/nouns');
 const verbs = require('./words/verbs');
+const ingverbs = require('./words/ingverbs');
+const groupings = require('./words/groupings');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
@@ -40,6 +42,20 @@ server.route({
     }
 });
 
+// Add the route for a full paragraph
+server.route({
+    method: 'POST',
+    path:'/scottbotspeech',
+    handler: function (request, reply) {
+        return reply({
+            "color": "green",
+            "message": getParagraph(),
+            "notify": false,
+            "message_format": "text"
+        });
+    }
+});
+
 // Start the server
 server.start((err) => {
     if (err) {
@@ -51,7 +67,7 @@ server.start((err) => {
 //get a business language string
 function getBusinessString() {
     var Scott = 'Scott says to '; //instantiate phrase
-    Scott += buildBusinessString(false) + '.';
+    Scott += buildBusinessString(false, false) + '.';
     return Scott;
 }
 
@@ -60,9 +76,26 @@ function getTodoList() {
     var numitems = Math.floor(Math.random() * 3) + 3; //Random number of items between 3-5
     var storyPoints = ['1/2', '1', '2', '3', '5', '8']; //Array of possible story points
     var Scott = 'Scott has assigned some stories to your squad:\n';
+
     //for each story, build a business string for it and assign a story point value for it
     for(var i = 0; i < numitems; i++) {
-        Scott += (i + 1) + ') ' + buildBusinessString(true) + '. (' + storyPoints[Math.floor(Math.random() * storyPoints.length)] + ')\n';
+        Scott += (i + 1) + ') ' + buildBusinessString(true, false) + '. (' + storyPoints[Math.floor(Math.random() * storyPoints.length)] + ')\n';
+    }
+
+    return Scott;
+}
+
+function getParagraph() {
+    var Scott = '';
+    var numsentences = Math.floor(Math.random() * 3) + 3; //random number of sentences between 3-5
+    var currentGroupings = groupings.slice(); //clone groupings since we're going to remove each grouping from the list as we use it
+
+    //for each sentence, use a grouping
+    for(var i = 0; i < numsentences; i++) {
+        var idx = Math.floor(Math.random() * currentGroupings.length); //index to get grouping from
+        var g = currentGroupings.splice(idx, 1)[0]; //get grouping at idx and remove it from the array so we don't repeat any groupings
+        Scott += g.phrase1.phrase + ' ' + buildBusinessString(false, g.phrase1.ing); //build first phrase and string with ing verb if needed
+        Scott += g.phrase2.phrase + ' ' + buildBusinessString(false, g.phrase2.ing) + '. '; //build second phrase and string with ing verb if needed
     }
 
     return Scott;
@@ -70,13 +103,20 @@ function getTodoList() {
 
 //builds the business language string.
 //if caps is true, then the first letter of the string will be capitalized.
-function buildBusinessString(caps) {
+function buildBusinessString(caps, ing) {
     var str = '';
+    //capitalize first letter if needed
     if(caps)
         str += adverbs[Math.floor(Math.random() * adverbs.length)].capitalizeFirstLetter() + ' '; //add adverb and space to phrase
     else
         str += adverbs[Math.floor(Math.random() * adverbs.length)] + ' '; //add adverb and space to phrase
-    str += verbs[Math.floor(Math.random() * verbs.length)] + ' '; //add verb and space to phrase
+
+    //use ing verbs if needed
+    if(ing)
+        str += ingverbs[Math.floor(Math.random() * ingverbs.length)] + ' '; //add verb and space to phrase
+    else
+        str += verbs[Math.floor(Math.random() * verbs.length)] + ' '; //add verb and space to phrase
+
     str += adjectives[Math.floor(Math.random() * adjectives.length)] + ' '; //add adjective and space to phrase
     str += nouns[Math.floor(Math.random() * nouns.length)]; //add noun to phrase
     return str;
---
+++ words/groupings.js
@@ -0,0 +1,105 @@
+/**
+ * Created by miles.wells on 6/9/2016.
+ */
+module.exports = [
+    {
+        phrase1: {
+            phrase: 'In order to',
+            ing: false
+        },
+        phrase2: {
+            phrase: ', we must',
+            ing: false
+        }
+    },
+    {
+        phrase1: {
+            phrase: 'Our main priority is',
+            ing: true
+        },
+        phrase2: {
+            phrase: ', but that doesn\'t mean we can\'t also',
+            ing: false
+        }
+    },
+    {
+        phrase1: {
+            phrase: 'I want us to',
+            ing: false
+        },
+        phrase2: {
+            phrase: '; however, we also need to',
+            ing: false
+        }
+    },
+    {
+        phrase1: {
+            phrase: 'Our company is',
+            ing: true
+        },
+        phrase2: {
+            phrase: ', but we\'re lacking in',
+            ing: true
+        }
+    },
+    {
+        phrase1: {
+            phrase: 'Our competition is',
+            ing: true
+        },
+        phrase2: {
+            phrase: ', but we can retaliate by',
+            ing: true
+        }
+    },
+    {
+        phrase1: {
+            phrase: 'The Dude is good at',
+            ing: true
+        },
+        phrase2: {
+            phrase: ', but we must also focus on',
+            ing: true
+        }
+    },
+    {
+        phrase1: {
+            phrase: 'Given our history of',
+            ing: true
+        },
+        phrase2: {
+            phrase: ', it\'s time we',
+            ing: false
+        }
+    },
+    {
+        phrase1: {
+            phrase: 'Once we',
+            ing: false
+        },
+        phrase2: {
+            phrase: ', we can',
+            ing: false
+        }
+    },
+    {
+        phrase1: {
+            phrase: 'If the people upstairs can',
+            ing: false
+        },
+        phrase2: {
+            phrase: ', I don\'t see why we can\'t',
+            ing: false
+        }
+    },
+    {
+        phrase1: {
+            phrase: 'To improve upon',
+            ing: true
+        },
+        phrase2: {
+            phrase: ', we must',
+            ing: false
+        }
+    }
+];
